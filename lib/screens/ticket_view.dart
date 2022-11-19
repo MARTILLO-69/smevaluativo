23430bf2d57b7728023239c0f2768b9447e427cf
Jheremy <chamy2406@gmail.com>
2022-11-18 21:23:59 -0400

    Jhere

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:smevaluativo/utils/app_layout.dart';
 import 'package:smevaluativo/utils/app_styles.dart';
 import 'package:smevaluativo/widgets/thick_container.dart';
@@ -17,6 +18,9 @@ class TicketView extends StatelessWidget {
         margin: const EdgeInsets.only(left: 16),
         child: Column(
           children: [
+            /*
+            showing the blue part of the card/ticket
+             */
             Container(
               decoration: BoxDecoration(
                 color: const Color(0xFF526799),
@@ -31,28 +35,71 @@ class TicketView extends StatelessWidget {
                       Text("NYC",style: Styles.headLineStyle3.copyWith(color: Colors.white),),
                       Expanded(child: Container()),
                       ThickContainer(),
-                      Expanded(child: SizedBox(
-                        height: 24,
-                        child: LayoutBuilder(
-                          builder: (BuildContext context , BoxConstraints constraints) {
-                            print("The wigth is ${constraints.constrainWidth()}");
-                            return Flex(
-                              direction: Axis.horizontal,
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              mainAxisSize: MainAxisSize.max,
-                              children: List.generate((constraints.constrainWidth()/6).floor(), (index) => SizedBox(
-                                  width: 3, height: 1, child: DecoratedBox(decoration: BoxDecoration(
-                                color: Colors.white //aca es lo ultimo
-                              ),),
-                              )),
-                            );
-                          },
-                        ),
+                      Expanded(child: Stack(
+                        children: [
+                          SizedBox(
+                            height: 24,
+                            child: LayoutBuilder(
+                              builder: (BuildContext context , BoxConstraints constraints) {
+                                print("The wigth is ${constraints.constrainWidth()}");
+                                return Flex(
+                                  direction: Axis.horizontal,
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: List.generate((constraints.constrainWidth()/6).floor(), (index) => SizedBox(
+                                    width: 3, height: 1, child: DecoratedBox(decoration: BoxDecoration(
+                                      color: Colors.white //aca es lo ultimo
+                                  ),),
+                                  )),
+                                );
+                              },
+                            ),
+                          ),
+                          Center(child: Transform.rotate(angle: 1.5,child: Icon(Icons.local_airport_rounded,color: Colors.white,),)),
+                        ],
                       )),
                       ThickContainer(),
+
                       Expanded(child: Container()),
-                      Text("London",style: Styles.headLineStyle3.copyWith(color: Colors.white),),
+
+                      Text("LDN",style: Styles.headLineStyle3.copyWith(color: Colors.white),),
                     ],
+                  ),
+                  const Gap(3),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: 100,child: Text('New-York',style: Styles.headLineStyle4.copyWith(color: Colors.white),),
+
+                      ),
+                      Text("8H 30M", style: Styles.headLineStyle4.copyWith(color: Colors.white),),
+                      SizedBox(
+                        width: 100,child: Text('London', textAlign:TextAlign.end,style: Styles.headLineStyle4.copyWith(color: Colors.white),),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            /*
+            showing the orange part of the card/ticket
+             */
+            Container(
+              color: const Color(0xFFF37B67),
+              child: Row(
+                children: [
+                  SizedBox(
+                    height: 20,
+                    width: 10,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(10),
+                          bottomRight: Radius.circular(10)
+                        )
+                      ),
+                    ),
                   )
                 ],
               ),
